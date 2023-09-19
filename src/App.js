277b6948a066a6f37ecea5614d9f3e59e0473d63
maliawanderer <malia.wanderer@gmail.com>
2023-09-18 22:35:16 -0400

    upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a
+          className="App-link"
+          href="/kanbas-app/KanbasNavigation/index.html"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Continue to Kanbas
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
